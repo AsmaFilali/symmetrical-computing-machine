1bdd176055007c8df0bfa8664fc2cb0295388016
AsmaFilali <46166141+AsmaFilali@users.noreply.github.com>
2025-01-29 19:14:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.querySelectorAll(".slides img");
+  slides.forEach(slide => (slide.style.display = "none"));
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
